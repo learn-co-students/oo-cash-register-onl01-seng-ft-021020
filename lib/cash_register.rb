19aeb0f3cde3cd116a2f492b9558406f70ccd09a
79ceca7e-1710-4439-8f8b-c115f316e9e9 <github-enterprise+79ceca7e-1710-4439-8f8b-c115f316e9e9@flatironschool.com>
2020-02-13 11:02:18 -0800

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,48 @@
+require 'pry'
 
+class CashRegister
+
+  attr_accessor :total, :last_item
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
+    @last_item = nil
+  end
+
+  def discount
+    @discount
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(title, price, quantity=1)
+    @last_item = price * quantity
+    @total += (price * quantity)
+    array_titles = Array.new(quantity, title)
+    array_titles.each { |item_title| @items << item_title }
+  end
+
+  def apply_discount
+    if self.discount != 
+      self.total = (self.total - (self.total * (self.discount.to_f/100))).to_i
+      "After the discount, the total comes to $#{self.total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    self.total -= @last_item
+  end
+
+
+
+end
