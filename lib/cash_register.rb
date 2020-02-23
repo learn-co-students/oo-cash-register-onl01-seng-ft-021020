837eaba6d9d96315bf7d15b46c47d2c6111989da
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-23 05:34:06 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,42 @@
+class CashRegister
 
+    attr_accessor :total, :discount 
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = [ ]
+    end
+
+    
+    def add_item(title, price, quantity = 1)
+      @price = price * quantity 
+      @total += @price 
+      if quantity > 1
+        x = 0 
+        while x < quantity
+          @items << title
+          x += 1
+        end
+      else 
+        @items << title 
+        end
+    end
+    
+    def items
+      @items
+    end 
+
+    def apply_discount
+      if @discount > 0 
+        @total -= (@total * @discount)/100
+        return "After the discount, the total comes to $#{@total}."
+      else 
+        return "There is no discount to apply."
+      end
+    end
+
+    def void_last_transaction
+        @total -= @price
+    end
+end
